d40b2aa8530cdbe1e82dd9d4c4325251461ea4c2
JURGEN <JURGEN@JURGEN-HP>
2018-08-09 13:11:05 -0500

    ok

---
+++ GMap.NET/GlobalAssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("MainSoft Technology")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+// Version information for an assembly consists of the following four values:
+// Major Version
+// Minor Version 
+// Build Number
+// Revision
+
+// Las siguientes 3 declaraciones dependerán de la política de versionamiento que adopte para cada uno de los assemblies generados.
+[assembly: AssemblyVersion("1.8.5.0")]
+[assembly: AssemblyFileVersion("1.8.5.0")]
+[assembly: AssemblyInformationalVersion("1.8.5.0")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
