a39a86b1c3a17accc19c65b956fdb70703ee88d5
W Hörchner <w.horchner@gmail.com>
2021-03-07 06:39:38 +0100

    In a multi threaded application I ran into an exception where one thread adds or removes markers from one or more layers  while the control is/was enumerating that collection of markers. This implementation of a Threadsafe Enumerator was inspirated by a CodeProject article and solves my situation. Altough the exception was very rare, a fatal at runtime and complex code in the mulit threaded application to avoid the issue is not really diserable.

---
+++ GMap.NET/GMap.NET.WindowsForms/GMap.NET.ObjectModel/ThreadSafeEnumerator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace GMap.NET.ObjectModel
+{
+    public class ThreadSafeEnumerator<T> : IEnumerator<T>
+    {
+        // this is the (thread-unsafe)
+        // enumerator of the underlying collection
+        private readonly IEnumerator<T> m_Inner;
+        // this is the object we shall lock on. 
+        private readonly object m_Lock;
+
+        public ThreadSafeEnumerator(IEnumerator<T> inner, object @lock)
+        {
+            m_Inner = inner;
+            m_Lock = @lock;
+            // entering lock in constructor
+            Monitor.Enter(m_Lock);
+        }
+
+        #region Implementation of IDisposable
+
+        public void Dispose()
+        {
+            // .. and exiting lock on Dispose()
+            // This will be called when foreach loop finishes
+            Monitor.Exit(m_Lock);
+        }
+
+        #endregion
+
+        #region Implementation of IEnumerator
+
+        // we just delegate actual implementation
+        // to the inner enumerator, that actually iterates
+        // over some collection
+
+        public bool MoveNext()
+        {
+            return m_Inner.MoveNext();
+        }
+
+        public void Reset()
+        {
+            m_Inner.Reset();
+        }
+
+        public T Current
+        {
+            get { return m_Inner.Current; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Current; }
+        }
+
+        #endregion
+    }
+}
